c8d79deaee6f1dca04b859538156b1636f9b6e88
Daniel Keating <dan.keating6@gmail.com>
2018-06-19 19:21:04 -0400

    Add/update frontend work

--- project/static/js/app.js
+++ project/static/js/app.js
@@ -36,72 +36,114 @@ angular.module('betaApp', ['ngRoute'])
                 });
         }
     ])
-    .controller('HeaderController', ($scope, $location) => {
+    // from http://jasonwatmore.com/post/2016/04/05/angularjs-jwt-authentication-example-tutorial
+    .run(function($rootScope, $http, $location, $localStorage) {
+            // keep user logged in after page refresh
+            if ($localStorage.currentUser) {
+                $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+            }
 
-        // for making active page link in navbar highlighted
-        $scope.isActive = (viewLocation) => {
-            return viewLocation === $location.path();
-        };
-
-    })
-
-.controller('IndexController', function() {
-    // TODO
-    // home page will load data from json file or storage
-    /*
-        $scope.message = 'Welcome to the home page!';
-
-            // this section needed to designate JSON, as otherwise interprets as XML and fails
-            $.ajaxSetup({
-                beforeSend: (xhr) => {
-                    if (xhr.overrideMimeType) {
-                        xhr.overrideMimeType("application/json");
-                    }
+            // redirect to login page if not logged in and trying to access a restricted page
+            $rootScope.$on('$locationChangeStart', function(event, next, current) {
+                var publicPages = ['/login'];
+                var restrictedPage = publicPages.indexOf($location.path()) === -1;
+                if (restrictedPage && !$localStorage.currentUser) {
+                    $location.path('/login');
                 }
             });
-            // load data
-            $.getJSON("data.json", { format: "json" }, { async: false })
-                .done((data) => {
-                    window.localStorage.setItem("data", JSON.stringify(data));
-                })
         }
-        */
-})
+        // end from
+        .controller('HeaderController', ($scope, $location) => {
 
-.controller('SignupController', [
-    '$scope', '$log', '$http',
-    function($scope, $log, $http) {
+            // for making active page link in navbar highlighted
+            $scope.isActive = (viewLocation) => {
+                return viewLocation === $location.path();
+            };
 
-        $scope.getNewUser = function() {
+        })
+
+        .controller('IndexController', function($location, AuthenticationService) {
             // TODO
-            $log.log($scope.user);
+            // from http://jasonwatmore.com/post/2016/04/05/angularjs-jwt-authentication-example-tutorial
+            var vm = this;
 
-            let userInput = $scope.user;
+            vm.login = login;
 
-            $http.post('/signup', { "user": userInput })
-                .then(function(results) {
-                    $log.log(results);
-                })
-                .catch(function(error) {
-                    $log.log(error);
+            initController();
+
+            function initController() {
+                // reset login status
+                AuthenticationService.Logout();
+            }
+
+            function login() {
+                vm.loading = true;
+                AuthenticationService.Login(vm.username, vm.password, function(result) {
+                    if (result === true) {
+                        $location.path('/');
+                    } else {
+                        vm.error = 'Username or password is incorrect';
+                        vm.loading = false;
+                    }
                 });
-        }
-    }
-])
+            }
+            // end from
 
-.controller('ProjectController', function() {
-    // TODO
-})
+            // home page will load data from json file or storage
+            /*
+                $scope.message = 'Welcome to the home page!';
 
-.controller('MessageController', function() {
+                    // this section needed to designate JSON, as otherwise interprets as XML and fails
+                    $.ajaxSetup({
+                        beforeSend: (xhr) => {
+                            if (xhr.overrideMimeType) {
+                                xhr.overrideMimeType("application/json");
+                            }
+                        }
+                    });
+                    // load data
+                    $.getJSON("data.json", { format: "json" }, { async: false })
+                        .done((data) => {
+                            window.localStorage.setItem("data", JSON.stringify(data));
+                        })
+                }
+                */
+        })
 
-    // let socket = io.connect('http://' + document.domain + ':' + location.port + '/')
+        .controller('SignupController', [
+            '$scope', '$log', '$http',
+            function($scope, $log, $http) {
 
-    // socket.on('connect', () => {
-    //     // connected
-    // })
-})
+                $scope.getNewUser = function() {
+                    // TODO
+                    $log.log($scope.user);
 
-.controller('IssueController', function() {
-    // TODO
-})
+                    let userInput = $scope.user;
+
+                    $http.post('/signup', { "user": userInput })
+                        .then(function(results) {
+                            $log.log(results);
+                        })
+                        .catch(function(error) {
+                            $log.log(error);
+                        });
+                }
+            }
+        ])
+
+        .controller('ProjectController', function() {
+            // TODO
+        })
+
+        .controller('MessageController', function() {
+
+            // let socket = io.connect('http://' + document.domain + ':' + location.port + '/')
+
+            // socket.on('connect', () => {
+            //     // connected
+            // })
+        })
+
+        .controller('IssueController', function() {
+            // TODO
+        })
